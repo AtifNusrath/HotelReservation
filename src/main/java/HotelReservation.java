780b076bd1df62a61e68f4b9bc0a8eb49ee5d215
atif00 <mohammedatif90@gmail.com>
2021-10-16 12:31:48 +0530

    ADD Hotel name and Hotel rate

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -18,17 +18,25 @@ class HotelReservation {
         count++;
     }
 
+    public static void display() {
+        for (Hotel data : hotels)
+            System.out.println(data);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
         boolean flag = true;
         while (true) {
-            System.out.println("Enter option 1.Add hotel\n 2. Exit\n");
+            System.out.println("Enter option 1.Add hotel\n 2. Display \n 3.Exit\n");
             int option = scanner.nextInt();
             switch (option) {
                 case 1:
                     addHotel();
                     break;
                 case 2:
+                    display();
+                    break;
+                case 3:
                     flag = false;
                     break;
             }
