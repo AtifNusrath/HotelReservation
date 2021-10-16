df456a93ae6b80e0b6c5397bd6ef08319aa800e3
atif00 <mohammedatif90@gmail.com>
2021-10-16 12:26:56 +0530

    ADD Hotel and Rate

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,19 @@
+public class Hotel {
+    int rating;
+    String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -1,6 +1,40 @@
+import java.util.*;
+
 class HotelReservation {
+
+    static int count;
+    static ArrayList<Hotel> hotels = new ArrayList<>();
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void addHotel() {
+        Hotel hotel = new Hotel();
+        System.out.println("Enter hotel name: ");
+        hotel.setName(scanner.next());
+        System.out.println("Enter hotel rate: ");
+        hotel.setRating(scanner.nextInt());
+        hotels.add(hotel);
+        System.out.println("Hotel Added");
+        count++;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
+        boolean flag = true;
+        while (true) {
+            System.out.println("Enter option 1.Add hotel\n 2. Exit\n");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    addHotel();
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+            }
+        }
+
+
     }
 }
 
